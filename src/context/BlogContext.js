44466f9f6208744401d55ac620d9a06344514a6c
akintola <akinlekan28@yahoo.com>
2019-12-27 15:11:22 +0100

    refactored app to use context helper

---
+++ src/context/BlogContext.js
@@ -0,0 +1,25 @@
+import createDataContext from "./createDataContext";
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_POST":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({
+      type: "ADD_POST"
+    });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
